d6e2d56f6b437a3f041c911c7095ef9cf1c9bd8f
Pantelimon Bogdan <pantelimonbogdanmihai92@gmail.com>
2025-04-01 12:31:27 +0300

    Udpates

--- fornt-end/src/Components/UserComponents/TelefoanePage.jsx
+++ fornt-end/src/Components/UserComponents/TelefoanePage.jsx
@@ -9,7 +9,6 @@ const[telefoane, setTelefoane]=useState([]);
 
 useEffect(()=> {
 fetch("https://api.devsite.cfd/telefoane-cards")
-console.log("Cerere pentru /telefoane-cards primită")
 .then((res)=>res.json())
 .then((data) => {
 console.log("Datele primite:", data);  // Verifică structura datelor

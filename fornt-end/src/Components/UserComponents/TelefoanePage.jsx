d6d3c71d16f8537539155cfb427e824b9f33bf48
Pantelimon Bogdan <pantelimonbogdanmihai92@gmail.com>
2025-04-01 13:04:00 +0300

    Updates

--- fornt-end/src/Components/UserComponents/TelefoanePage.jsx
+++ fornt-end/src/Components/UserComponents/TelefoanePage.jsx
@@ -17,30 +17,36 @@ setTelefoane(data);
 .catch((err) => console.log("Eroare la fetch pt telefoane page",err))
 },[]);
 
+console.log("Se renderizează pagina de telefoane");
+
 return (
-<div className="telefoane-container">
-<Header />
-<main>
-<div className="hero-div-telefoanePage">
-      <div className="hero-text-Telefoane"> {/* corectat className */}
-      <h1 className="hero-title-Telefoane">Telefoane</h1>
+  <div className="telefoane-container">
+    <Header />
+    <main>
+      <div className="hero-div-telefoanePage">
+        <div className="hero-text-Telefoane">
+          <h1 className="hero-title-Telefoane">Telefoane</h1>
+        </div>
       </div>
-</div>
-<div className="product-cards-div">
-      {telefoane.map((produs) => (
-        <CardTelefoanePage
-          key={produs.id}
-          productId={produs.id}
-          imgURL={produs.image_url}
-          name={produs.name}
-          price={produs.price}
-          category={produs.category}
-        />
-      ))}
-    </div>
-</main>
-<Footer />
-</div>
+      <div className="product-cards-div">
+        {telefoane.length === 0 ? (
+          <p>Se încarcă telefoanele sau nu există produse disponibile...</p>
+        ) : (
+          telefoane.map((produs) => (
+            <CardTelefoanePage
+              key={produs.id}
+              productId={produs.id}
+              imgURL={produs.image_url}
+              name={produs.name}
+              price={produs.price}
+              category={produs.category}
+            />
+          ))
+        )}
+      </div>
+    </main>
+    <Footer />
+  </div>
 );
 }
 

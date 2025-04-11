9e9eed8d297ea5cb41aa8eb8186b709f4e1c64c5
Pantelimon Bogdan <pantelimonbogdanmihai92@gmail.com>
2025-04-11 22:27:27 +0300

    Updates

--- fornt-end/src/Components/UserComponents/Homepage.jsx
+++ fornt-end/src/Components/UserComponents/Homepage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Header from "../UserComponents/Header.jsx";
 import Footer from "./Footer.jsx";
 import CardHomePage from "./CardHomePage.jsx";
@@ -18,12 +18,29 @@ function CreateCard (info) {
 
 function HomePage() {
 
+const [bgIndex, setBgIndex]= useState(0);
+
+const backgroundImagesHeroDiv = [
+"https://www.ookla.com/s/media/2024/11/iPhone-16-5G-Performance-Header-2024.png",
+"https://images.mlssoccer.com/image/private/t_editorial_landscape_8_desktop_mobile/mls/ezu7nroj7ucmx78qe3su.jpg",
+"https://www.apple.com/newsroom/images/product/imac/standard/apple_new-imac-spring21_hero_04202021_Full-Bleed-Image.jpg.large.jpg"
+]
+
+
+useEffect(()=>{
+const interval = setInterval (() => {setBgIndex((prevIndex)=>(prevIndex+1) % backgroundImagesHeroDiv.length)} , 5000);
+
+return () => clearInterval(interval)
+
+},[])
+
   return (
     <div className="telefoaneHero-container">
       <Header />
       <main className="main-content">
         <div className="hero-div">
-          <div className="hero-text"> {/* corectat className */}
+          <div className="hero-text"
+          style={{background: `url(${backgroundImagesHeroDiv[bgIndex]})`}}> 
             <h1 className="titleHomePageHero">Bun venit pe site-ul nostru</h1>
             <h2 className="subtitleHomePageHero">Descoperă cele mai bune oferte</h2>
           </div>

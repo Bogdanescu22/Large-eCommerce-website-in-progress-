97901210d4c4f4b749ebffb87ed2e513087b7522
Pantelimon Bogdan <pantelimonbogdanmihai92@gmail.com>
2025-04-28 13:56:30 +0300

    Updates

--- fornt-end/src/Components/AdminComponents/AdminPasswordResetPage.jsx
+++ fornt-end/src/Components/AdminComponents/AdminPasswordResetPage.jsx
@@ -4,7 +4,6 @@ import { useParams, useNavigate } from "react-router-dom";
 
 const AdminPasswordResetPage = () => {
   const [newPassword, setNewPassword] = useState("");
-  const [resetPassword, setResetPassword] = useState(false);
   const { resetToken } = useParams();
   const navigate = useNavigate();
 
@@ -18,18 +17,14 @@ const AdminPasswordResetPage = () => {
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
-        setResetPassword(true);
+        navigate("/admin/reset-password/succes");
       })
       .catch((error) => {
         console.error("Eroare la fetch-ul pt resetarea parolei:", error);
       });
   };
 
-  useEffect(() => {
-    if (resetPassword) {
-      navigate("https://www.devsite.cfd/admin/reset-password/succes");
-    }
-  }, [resetPassword, navigate]);
+
 
   return (
     <div className="admin-password-reset-page">

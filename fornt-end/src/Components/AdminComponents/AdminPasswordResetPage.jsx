3b041524ac909ffa3eb3e50c329d0f62ddff6c78
Pantelimon Bogdan <pantelimonbogdanmihai92@gmail.com>
2025-04-28 14:02:17 +0300

    Updates

--- fornt-end/src/Components/AdminComponents/AdminPasswordResetPage.jsx
+++ fornt-end/src/Components/AdminComponents/AdminPasswordResetPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import AdminHeader from "./AdminHeader";
 import { useParams, useNavigate } from "react-router-dom";
 

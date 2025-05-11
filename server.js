8d1e21e8ee40b27275fd287215b9cc60cae9b271
Pantelimon Bogdan <pantelimonbogdanmihai92@gmail.com>
2025-05-11 21:39:17 +0300

    Updates

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ const profilePictureUpload = multer({
   storage: multerS3({
     s3: s3,
     bucket: 'my-app-uploads-devsite',
-    acl: undefined,
+    acl: null,
     key: function (req, file, cb) {
       cb(null, `profile-pictures/${req.params.user_id}-${Date.now()}.webp`);
     }
